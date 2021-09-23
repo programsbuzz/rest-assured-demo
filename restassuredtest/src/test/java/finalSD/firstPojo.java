3d1bf6d8e5c69c4c14032b69779342592c377d2a
arili <arili@LAPTOP-3MABV6QD>
2021-09-23 19:58:18 +0530

    Updated Working Serial/Deserial Package Name Is finalSD

--- restassuredtest/src/test/java/finalSD/firstPojo.java
+++ restassuredtest/src/test/java/finalSD/firstPojo.java
@@ -2,6 +2,8 @@ package finalSD;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class firstPojo {
 private String meta;
 private List<dataPojo> data;
@@ -15,6 +17,7 @@ public void setMeta(String meta) {
 public List<dataPojo> getData() {
 	return data;
 }
+@JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
 public void setData(List<dataPojo> data) {
 	this.data = data;
 }
